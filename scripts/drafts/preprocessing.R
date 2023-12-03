8f753907329b495e94a9f855d1661ab918fd4304
The boyhood <95021816+YixinXue@users.noreply.github.com>
2023-12-02 21:30:34 -0800

    update

--- scripts/drafts/preprocessing.R
+++ scripts/drafts/preprocessing.R
@@ -15,7 +15,9 @@ data <- data %>%
          dist_last_transact = distance_from_last_transaction,
          ratio_to_med_price = ratio_to_median_purchase_price) %>%
   mutate(across(.cols = c(dist_home, dist_last_transact, ratio_to_med_price), 
-                ~ scale(.x)))
+                ~ trim(scale((.x))[, 1], .at = 3)))
 
 head(data)  
 dim(data)
+
+save(list = 'data', file = 'data/processed/data.RData')
