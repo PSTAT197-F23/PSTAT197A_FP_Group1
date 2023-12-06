e28f14275016a80d33fc17d9fa99a63229e55e45
Amber Wang <Roseamberwang@gmail.com>
2023-12-05 21:18:35 -0800

    updated EDA

--- scripts/drafts/EDA.R
+++ scripts/drafts/EDA.R
@@ -1,4 +1,4 @@
-#Yixin Xue
+#Yixin Xue + Amber Wang
 
 library(readr)
 library(ggplot2)
@@ -7,10 +7,28 @@ library(corrplot)
 
 data <- read_csv("data/card_transdata.csv") 
 head(data)
-dim(data)
 
-# The dataset contains 1000000 observations
+dim(data) # The dataset contains 1000000 observations
 
+# Missing Value
+sum(is.na(data)) # no missing data
+
+
+# distribution and outliers in each feature
+par(mfrow = c(3, 3))
+
+for (col in colnames(data)) {
+  hist(data[[col]], main = col, xlab = col, col = "skyblue", border = "black", breaks = 20)
+}   # plot histograms
+
+for (col in colnames(data)) {
+  density_values <- density(data[[col]])
+  plot(density_values, main = col, col = "skyblue", lwd = 2)
+}  # Plot the density curve
+
+par(mfrow = c(1, 1))
+
+# Fraud Distribution
 data %>%
   ggplot(aes(x = fraud)) +
   geom_bar() +
@@ -21,6 +39,8 @@ data %>%
   group_by(fraud) %>%
   count()
 
+
+
 # We noticed that most transactions (over 90%) are not fraud. So accuracy may not be a proper model evaluation metric. 
 # Consider AUC-ROC, f1 score as evaluation metric.
 # Consider using stratified sampling/oversampling to handle the imbalanced classification problem
@@ -29,6 +49,12 @@ data %>%
 # data %>%
 #   smote(form = fraud ~ ., data = ., perc.over = 200, k = 5)
 
+
+# Correlation
 data %>%
   cor(use = 'everything') %>%
   corrplot(type = 'lower')
+
+
+
+
